8dc486c34d0b71d1a5913c5b0e39820c49ac35e3
unknown <shiveshtiwari0@gmail.com>
2024-07-30 14:37:46 +0530

    solving markdown path problem

--- app/createReadme/page.tsx
+++ app/createReadme/page.tsx
@@ -3,7 +3,7 @@ import path from 'path';
 import CreateReadmePage from '@/components/pages/CreateReadmePage';
 
 const CreateReadme = () => {
-    const markdownPath = path.resolve(process.cwd() + "/markdowns/README");
+    const markdownPath = path.resolve(process.cwd() + "/app/markdowns/README");
     console.log(`current: ${markdownPath}`);
     const files = fs.readdirSync(markdownPath);
     
