22398c5c60dc8f89df20e74b602b8063f84213ac
unknown <shiveshtiwari0@gmail.com>
2024-07-30 14:30:50 +0530

    solving markdown path problem

--- actions/getFileData.ts
+++ actions/getFileData.ts
@@ -1,8 +1,9 @@
 "use server";
 
 import { promises as fs } from "fs";
+import path from "path";
 
 export async function getFileData(filePath:string) {
-    const file = await fs.readFile(filePath, "utf-8");
+    const file = await fs.readFile(path.resolve(filePath), "utf-8");
     return file;
 }
